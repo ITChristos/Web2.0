3816ca142495a85797873704936ece63caf7a4e3
ITChristos <georjiu@hotmail.com>
2021-04-24 13:28:33 -0700

    Add accepted Charset

--- action_page.php
+++ action_page.php
@@ -4,7 +4,7 @@ $serverName = "DEVICE-DESKTOP\SQLEXPRESS"; //serverName\instanceName
 
 // Since UID and PWD are not specified in the $connectionInfo array,
 // The connection will be attempted using Windows Authentication
-$connectionInfo = array("Database" => "Web", "UID" => "mwright", "PWD" => "Pa55w.rd");
+$connectionInfo = array("Database" => "Web", "CharacterSet" => "UTF-8");
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
 if ($conn) {
